b2484004400515cb7a32bd7666e6f97f708f7f93
trizna3 <adam.trizna32@gmail.com>
2020-07-11 20:26:23 +0200

    v2 data paths corrected.

--- sources/utils.py
+++ sources/utils.py
@@ -2,8 +2,8 @@ import pandas as pd
 
 TRAIN_PATH_v1 = "../data/v1/training.tsv"
 TEST_PATH_v1 = "../data/v1/dev.tsv"
-TRAIN_PATH_v2 = "../data/v2/training.tsv"
-TEST_PATH_v2 = "../data/v2/dev.tsv"
+TRAIN_PATH_v2 = "../data/v2/training_v2.tsv"
+TEST_PATH_v2 = "../data/v2/dev_v2.tsv"
 
 KEY_TEXT = "tweet_text"
 KEY_TWEET_ID = "tweet_id"
