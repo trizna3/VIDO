9b754258a10a4241cec02f47ff5600b2170971c5
kyselica12 <dano.kyselica@gmail.com>
2020-08-26 16:13:49 +0200

    confusion matrix added

--- sources/fasttext.py
+++ sources/fasttext.py
@@ -2,6 +2,12 @@ import os
 import subprocess
 from sources.model import *
 from sources.utils import *
+from sklearn.metrics import confusion_matrix
+
+import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy as np
+
 
 
 class FastText(Model):
@@ -18,7 +24,7 @@ class FastText(Model):
         #    universal_newlines=True)
 
         stream = os.popen(
-            f'../fastText/fasttext supervised -minCount 2 -wordNgrams 3 -minn 3 -maxn 8 -lr 0.7 -dim 100 -epoch 25 f-input ./{self.name}.train f-output ./{self.name}.model')
+            f'../fastText/fasttext supervised -minCount 2 -wordNgrams 3 -minn 3 -maxn 8 -lr 0.7 -dim 100 -epoch 25  -input ./{self.name}.train -output ./{self.name}.model')
 
         output = stream.read()
         print(output)
@@ -36,25 +42,17 @@ class FastText(Model):
         #         break
 
     def run(self, data):
-        # process = subprocess.Popen(['../fastText/fasttext', f'test ./{self.name}.model', f'./{self.name}.valid','3'], 
-        #                    stdout=subprocess.PIPE,
-        #                    universal_newlines=True)
 
-        # while True:
-        #     output = process.stdout.readline()
-        #     print(output.strip())
-        #     # Do something else
-        #     return_code = process.poll()
-        #     if return_code is not None:
-        #         print('RETURN CODE', return_code)
-        #         # Process has finished, read rest of the output 
-        #         for output in process.stdout.readlines():
-        #             print(output.strip())
-        #         break
+        with open(f'{self.name}.run', 'w') as f:
+            for x in data:
+                self.print_sequence(x, f)
+        
 
-        stream = os.popen(f'../fastText/fasttext test ./{self.name}.model ./{self.name}.valid 3')
+        stream = os.popen(f'../fastText/fasttext predict ./{self.name}.model.bin ./{self.name}.run')
         output = stream.read()
-        print(output)
+
+        return output
+
 
     def train(self, train_data_path):
         train_data = read_datafile(train_data_path)
@@ -79,10 +77,26 @@ class FastText(Model):
         data = '__label__' + test_data['claim'].astype(str) + test_data['check_worthiness'].astype(str) + ' ' + \
                test_data['tweet_text']
 
-        with open(f'{self.name}.valid', 'w') as f:
-            for x in data:
-                self.print_sequence(x, f)
-        self.run(data)
+        output = self.run(data)
+
+        df = pd.DataFrame()
+        df["predicted"] = output.split()
+        df["labeled"] = [d.split()[0] for d in data]
+
+        cm = confusion_matrix(df["labeled"], df["predicted"], labels=['__label__11','__label__10','__label__00'])
+
+        ax= plt.subplot()
+        sns.heatmap(cm, annot=True, ax = ax); #annot=True to annotate cells
+
+        ax.set_xlabel('Predicted labels');ax.set_ylabel('True labels'); 
+        ax.set_title('Confusion Matrix'); 
+        ax.xaxis.set_ticklabels(['__label__11','__label__10','__label__00']); ax.yaxis.set_ticklabels(['__label__11','__label__10','__label__00']);
+
+        plt.show()
+
+        return np.sum(cm.diagonal()) / np.sum(cm)
+
+
 
     def get_result_path(self):
         return DEFAULT_RESULT_PATH.format(self.name)
